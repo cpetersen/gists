e3973f28508edc2fcc3105a89a65ed55ea754ada
Christopher Petersen <chris@petersen.io>
2019-02-28 09:11:04 -0800

    Handling pagination

--- gists.rb
+++ gists.rb
@@ -4,19 +4,31 @@ require 'git'
 require 'json'
 require 'restclient'
 
-gists = JSON.parse(RestClient.get('https://api.github.com/users/cpetersen/gists'))
-gists.each do |gist|
-  begin
-    if File.exists? "#{gist["id"]}/.git"
-      puts "Pulling... #{gist["html_url"]}"
-      g = Git.open(gist["id"])
-      g.pull
-    else
-      puts "Cloning... #{gist["html_url"]}"
-      FileUtils.mkdir_p(gist["id"])
-      Git.clone(gist["git_pull_url"], gist["id"])
+username = ARGV.first
+puts "Getting gists for #{username}"
+
+user_dir = "repos/#{username}"
+FileUtils.mkdir_p(user_dir) unless File.exists?(user_dir)
+
+page = 0
+while true
+  page+=1
+  puts "Getting page #{page}..."
+  gists = JSON.parse(RestClient.get("https://api.github.com/users/#{username}/gists?page=#{page}"))
+  return if gists.empty?
+  gists.each do |gist|
+    repo_dir = "#{user_dir}/#{gist["id"]}"
+    begin
+      if File.exists? "#{repo_dir}/.git"
+        puts "Pulling... #{gist["html_url"]}"
+        g = Git.open(repo_dir)
+        g.pull
+      else
+        puts "Cloning... #{gist["html_url"]}"
+        Git.clone(gist["git_pull_url"], repo_dir)
+      end
+    rescue => e
+      puts "Error on #{gist["html_url"]} [#{e.message}]"
     end
-  rescue => e
-    puts "Error on #{gist["html_url"]} [#{e.message}]"
   end
-end; nil
+end
